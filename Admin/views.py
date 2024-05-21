4316ae2ce7269e13f96f2beb466d02a62336dc7f
boopathyganesh <boopathyganesh95@gmail.com>
2024-05-21 21:27:12 +0530

    Debugging

--- Admin/views.py
+++ Admin/views.py
@@ -12,7 +12,6 @@ from .forms import LoginForm
 import subprocess,hashlib,hmac
 
 
-
 cognito = boto3.client('cognito-idp',region_name=os.getenv('AWS_REGION'),
                           aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                           aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
@@ -21,6 +20,10 @@ s3 = boto3.client('s3',region_name=os.getenv('AWS_REGION'),
                           aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 user_pool_id = settings.AWS_COGNITO_USER_POOL_ID
 bucket_name = 'value1-admindashboard'
+
+print("AWS_REGION:", os.getenv('AWS_REGION'))
+print("AWS_ACCESS_KEY_ID:", os.getenv('AWS_ACCESS_KEY_ID'))
+print("AWS_SECRET_ACCESS_KEY:", os.getenv('AWS_SECRET_ACCESS_KEY'))
 
 def test(request):
     return render(request, 'test.html',{'msg': 'Webhooks working perfectly!','text':'CI/CD pipeline will be automated! Auth Added!'})
